e587d5207dd789a74da88f518f5dcb8eee30f570
reneang17 <reneang17@gmail.com>
2025-09-26 13:50:19 -0600

    Update vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  base: command === "build" ? "/pdf-viewer-poc/" : "/",
+  build: {
+    outDir: "dist",
+  },
+}));
